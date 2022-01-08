892f2ffdf570a7c50f55a676fa8f18cdfc5bfa1a
jabrena <bren@juanantonio.info>
2022-01-08 22:41:46 +0000

    Upgrading examples to latest spring-boot version

--- thymeleaf/thymeleaf-forms/src/main/java/com/attacmosian/thymeleaf/forms/controllers/ProjectController.java
+++ thymeleaf/thymeleaf-forms/src/main/java/com/attacmosian/thymeleaf/forms/controllers/ProjectController.java
@@ -3,12 +3,20 @@ package com.attacmosian.thymeleaf.forms.controllers;
 import com.attacmosian.thymeleaf.forms.domains.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProjectController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 
     @GetMapping("/create-project")
     public String createProjectForm(Model model) {
